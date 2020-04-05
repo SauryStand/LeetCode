5119bc5f11930debf29de39aa73fc4671bbdd5ee
SauryStand <voyager2511@outlook.com>
2020-04-05 17:13:34 +0100

    update:144

---
+++ src/main/python/binaryTree/binaryTreePreorderTraversal144.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        ret = []
+        stack = [root] # 用栈的方式实现
+        while stack:
+            node = stack.pop()
+            if node:
+                ret.append(node.val)
+                stack.append(node.right)
+                stack.append(node.left)
+        return ret
